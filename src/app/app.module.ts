f376f07d61bdcbc23609939aece81811e8dd435f
rihab <rihab.attig@gmail.com>
2019-05-13 18:42:11 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from'./material'
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MaterialModule } from './material';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { Http, HttpModule } from '@angular/http';
 import { VoisinsComponent } from './voisins/voisins.component';
-import { ProfilComponent } from './profil/profil.component'
+import { ProfilComponent } from './profil/profil.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemandesComponent } from './demandes/demandes.component';
 import { MenuComponent } from './menu/menu.component';
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,36 +11,35 @@ import { PublicationService } from '../publication.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  connect: boolean
-  user: SimpleUser = new SimpleUser()
+  connect: boolean;
+  user: SimpleUser = new SimpleUser();
   notification: any = [];
   nbreNotification = 0;
 
   constructor(private router: Router, private param: ParamService, private publicationService: PublicationService) { }
 
   ngOnInit() {
-    
-    let user = JSON.parse(localStorage.getItem("connection"));
+
+    const user = JSON.parse(localStorage.getItem('connection'));
     console.log(user);
-    if (user==true) {
-      this.connect= true;
-      console.log("ghjds;gf");
-    }
-    else this.connect=false;
-    console.log("Connect from menu :" + this.connect)
+    if (user === true) {
+      this.connect = true;
+      console.log('ghjds;gf');
+    } else { this.connect = false; }
+    console.log('Connect from menu :' + this.connect);
     this.user = this.param.getActifUser();
     this.publicationService.getApplicationByIdUser(this.user.id).subscribe(
       data => {
       this.notification = data.json();
-        for (var i = 0; i < this.notification.length; i++) {
-          if (this.notification[i].readApp == false) {
-            console.log("Read==="+this.notification[i].readApp)
+        for (let i = 0; i < this.notification.length; i++) {
+          if (this.notification[i].readApp === false) {
+            console.log('Read===' + this.notification[i].readApp);
              this.nbreNotification++; }
         }
       }
-    )
+    );
   }
-  
+
   navigate1(route: string) {
     this.router.navigate([route]);
 
@@ -50,17 +49,17 @@ export class MenuComponent implements OnInit {
     this.router.navigate(['sign']);
 
   }
-  goToMyProfil(){
-    
-    this.param.setprofilUser(this.param.getActifUser())
+  goToMyProfil() {
+
+    this.param.setprofilUser(this.param.getActifUser());
     this.router.navigate(['profil']);
 
   }
 
   deconnecter() {
     this.connect = false;
-    this.param.setConnect(false)
-    this.param.setActifUser(new SimpleUser())
+    this.param.setConnect(false);
+    this.param.setActifUser(new SimpleUser());
     localStorage.clear();
   }
 
